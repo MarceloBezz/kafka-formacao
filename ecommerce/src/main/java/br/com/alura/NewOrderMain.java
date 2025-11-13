a5342eccf461224a71ec92a5ff5f63f7bafb847d
Marcelo <mdbezerra16@gmail.com>
2025-11-12 21:25:35 -0300

    refactor: :recycle: Refatoração da inicialização de produtores e consumidores

--- ecommerce/src/main/java/br/com/alura/NewOrderMain.java
+++ ecommerce/src/main/java/br/com/alura/NewOrderMain.java
@@ -1,46 +1,15 @@
 package br.com.alura;
 
-import java.util.Properties;
 import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 
-import org.apache.kafka.clients.producer.Callback;
-import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.clients.producer.ProducerRecord;
-import org.apache.kafka.common.serialization.StringSerializer;
-
 public class NewOrderMain {
-    @SuppressWarnings("resource")
     public static void main(String[] args) throws InterruptedException, ExecutionException {
-        var producer = new KafkaProducer<String, String>(properties());
-        var key = UUID.randomUUID().toString();
-
-        var value = key + "132123,23566,9458305";
-        var record = new ProducerRecord<>("ECOMMERCE_NEW_ORDER", value, value);
-        
-        var email = key + "Thank you for your order! We are processing your order!";
-        var emailRecord = new ProducerRecord<>("ECOMMERCE_SEND_EMAIL", email, email);
-        
-        producer.send(record, callback()).get();
-        producer.send(emailRecord, callback()).get();
-    }
-
-    private static Callback callback() {
-        return (data, ex) -> {
-            if (ex != null) {
-                ex.printStackTrace();
-                return;
-            }
-            System.out.println("Sucesso enviando " + data.topic() + ":::partition " + data.partition() + "/ offset " + data.offset() + "/ timestamp " + data.timestamp());
-        };
-    }
-
-    private static Properties properties() {
-        var properties = new Properties();
-        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
-        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        return properties;
+        var dispatcher = new KafkaDispatcher();        
+        for (int i = 0; i < 11; i++) {
+            var key = UUID.randomUUID().toString();
+            var value = key + "132123,23566,9458305";
+            dispatcher.send("ECOMMERCE_NEW_ORDER", key, value);
+        } 
     }
 }
