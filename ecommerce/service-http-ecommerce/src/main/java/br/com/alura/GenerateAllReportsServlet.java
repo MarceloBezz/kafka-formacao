64f5a0a21fa50bfc31ae779a3e2d001d2f65045f
Marcelo <mdbezerra16@gmail.com>
2025-11-21 13:15:04 -0300

    feat: :sparkles: Adição do servlet para geração de relatórios e serviço de envio de mensagens em lote

---
+++ ecommerce/service-http-ecommerce/src/main/java/br/com/alura/GenerateAllReportsServlet.java
@@ -0,0 +1,26 @@
+package br.com.alura;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class GenerateAllReportsServlet extends HttpServlet {
+
+    private final KafkaDispatcher<String> batchDispatcher = new KafkaDispatcher<String>();
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try {
+            batchDispatcher.send("SEND_MESSAGE_TO_ALL_USERS", "USER_GENERATE_READING_REPORT", "USER_GENERATE_READING_REPORT");
+
+            System.out.println("Sent generated reports to all users!");
+            resp.setStatus(200);
+            resp.getWriter().println("Report requests generated");
+        } catch (Exception e) {
+            throw new ServletException(e);
+        }
+    }
+}
---
+++ ecommerce/service-users/src/main/java/br/com/alura/BatchSendMessageService.java
@@ -0,0 +1,55 @@
+package br.com.alura;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+
+public class BatchSendMessageService {
+    private final Connection connection;
+
+    BatchSendMessageService() throws SQLException {
+        String url = "jdbc:sqlite:user_database.db";
+        this.connection = DriverManager.getConnection(url);
+        connection.createStatement().execute("""
+                create table if not exists Users (
+                uuid varchar(200) primary key,
+                email varchar(200))""");
+    }
+
+    public static void main(String[] args) throws SQLException {
+        var batchService = new BatchSendMessageService();
+        var service = new KafkaService<String>(BatchSendMessageService.class.getSimpleName(),
+                "SEND_MESSAGE_TO_ALL_USERS",
+                batchService::parse,
+                String.class,
+                Map.of());
+        service.run();
+    }
+
+    private final KafkaDispatcher<User> userDispatcher = new KafkaDispatcher<>();
+
+    void parse(ConsumerRecord<String, String> record) throws InterruptedException, ExecutionException, SQLException {
+        System.out.println("------------------------------------------");
+        System.out.println("Processing new batch");
+        System.out.println("Topic" + record.value());
+
+        for (User user : getAllUsers()) {
+            userDispatcher.send(record.value(), user.getUuid(), user);
+        }
+    }
+
+    private List<User> getAllUsers() throws SQLException {
+        var results = connection.prepareStatement("select uuid from Users").executeQuery();
+        List<User> users = new ArrayList<>();
+        while (results.next()) {
+            users.add(new User(results.getString(1)));
+        }
+        return users;
+    }
+}
