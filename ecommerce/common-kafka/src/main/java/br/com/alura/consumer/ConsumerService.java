db64cd5f0ae9142857eeab1ee381e53c30ab6dfc
Marcelo <mdbezerra16@gmail.com>
2025-11-23 11:57:54 -0300

    build: :zap: Implementação do ConsumerService no EmailNewOrderService

--- ecommerce/common-kafka/src/main/java/br/com/alura/consumer/ConsumerService.java
+++ ecommerce/common-kafka/src/main/java/br/com/alura/consumer/ConsumerService.java
@@ -2,13 +2,14 @@ package br.com.alura.consumer;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.util.concurrent.ExecutionException;
 
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 
 import br.com.alura.Message;
 
 public interface ConsumerService<T> {
-    void parse(ConsumerRecord<String, Message<T>> record) throws URISyntaxException, IOException;
+    void parse(ConsumerRecord<String, Message<T>> record) throws URISyntaxException, IOException, InterruptedException, ExecutionException;
     String getTopic();
     String getConsumerGroup();
 }
--- ecommerce/service-email-new-order/src/main/java/br/com/alura/EmailNewOrderService.java
+++ ecommerce/service-email-new-order/src/main/java/br/com/alura/EmailNewOrderService.java
@@ -1,35 +1,42 @@
 package br.com.alura;
 
-import java.util.Map;
 import java.util.concurrent.ExecutionException;
 
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 
-import br.com.alura.consumer.KafkaService;
+import br.com.alura.consumer.ConsumerService;
+import br.com.alura.consumer.ServiceRunner;
 import br.com.alura.dispatcher.KafkaDispatcher;
 
-public class EmailNewOrderService {
+public class EmailNewOrderService implements ConsumerService<Order>{
     public static void main(String[] args) throws InterruptedException, ExecutionException {
-        var emailService = new EmailNewOrderService();
-        var service = new KafkaService<String>(EmailNewOrderService.class.getSimpleName(),
-               "ECOMMERCE_NEW_ORDER",
-                emailService::parse,
-                Map.of());
-        service.run();
+        new ServiceRunner<>(EmailNewOrderService::new).start(1);
     }
 
     private final KafkaDispatcher<String> emailDispatcher = new KafkaDispatcher<>();
 
-    void parse(ConsumerRecord<String, Message<String>> record) throws InterruptedException, ExecutionException {
+    public void parse(ConsumerRecord<String, Message<Order>> record) throws InterruptedException, ExecutionException {
         System.out.println("------------------------------------------");
         System.out.println("Processing new order, preparing email");
-        System.out.println(record.value());
+        var message = record.value();
+        System.out.println(message);
     
+        var order = message.getPayload();
         var emailCode = "Thank you for your order! We are processing your order!";
         emailDispatcher.send("ECOMMERCE_SEND_EMAIL",
-                record.value().getPayload(),
+                order.getEmail(),
                 emailCode,
                 record.value().getId().continueWith(EmailNewOrderService.class.getSimpleName()));
+    }
+
+    @Override
+    public String getTopic() {
+        return "ECOMMERCE_NEW_ORDER";
+    }
+
+    @Override
+    public String getConsumerGroup() {
+        return EmailNewOrderService.class.getSimpleName();
     }
 
 }
